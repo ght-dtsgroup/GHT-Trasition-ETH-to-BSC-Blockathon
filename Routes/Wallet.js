1213846a21a6c16b88524dd4c96a50d1762d0a30
KhoaGit <48850888+KhoaGit@users.noreply.github.com>
2021-05-24 15:13:31 +0700

    Delete Routes directory

--- Routes/Wallet.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(app) {
-    let walletCtrl = require('../Controllers/WalletController');
-  
-    app.route('/user/:userId')
-      .get(walletCtrl.create);
-
-    app.route('/setWhiteList/:userAddress/:flag')
-      .get(walletCtrl.set);
-  };
