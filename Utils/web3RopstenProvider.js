81b83a047606c13f64b79d01b844c614896e9441
KhoaGit <48850888+KhoaGit@users.noreply.github.com>
2021-05-24 15:13:45 +0700

    Delete Utils directory

--- Utils/web3RopstenProvider.js
+++
@@ -1,6 +0,0 @@
-/*const Web3 = require("web3");
-const fs = require("fs");
-
-const INFURA_ID = process.env.INFURA_ID;
-
-exports.web3 = new Web3(new Web3.providers.HttpProvider(`https://ropsten.infura.io/v3/${INFURA_ID}`));*/
